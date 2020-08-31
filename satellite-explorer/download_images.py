80a0d622d4e17ae38430665baca9ea0c25554a2b
Nicky Forster <nforster@ap.org>
2020-08-31 12:05:15 -0400

    Updated login process for earthdata

--- satellite-explorer/download_images.py
+++ satellite-explorer/download_images.py
@@ -8,16 +8,21 @@ class DownloadSession:
         self.password = app.config['EARTHDATA_PASS']
     
     def download_granule(self, url):
-        resp = requests.get(url, stream=True, auth=(self.username, self.password))
-        print(resp.status_code)
-        if resp.status_code != 200:
-            print("not downloaded. Verify that your username and password are correct")
-        else:
-            total_length = int(resp.headers.get('content-length'))
-            dl = 0
-            with open(f'{app.root_path}/data/temp.hdf', 'wb') as outfile:
-                for data in resp.iter_content(chunk_size=16384):
-                    dl += len(data)
-                    outfile.write(data)
-                    done = int(dl / total_length * 100)
-                    print(f'{done}%', end='\r')
+    ## https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
+        with requests.Session() as session:
+            session.auth = (self.username, self.password)
+            r1 = session.request('get', url)
+            r = session.get(r1.url, auth=(self.username, self.password), stream=True)
+            if r.status_code != 200:
+                print(r.content)
+                print("not downloaded. Verify that your username and password are correct")
+            elif r.ok:
+                print("Got the data...")
+                total_length = int(r.headers.get('content-length'))
+                dl = 0
+                with open(f'{app.root_path}/data/temp.hdf', 'wb') as outfile:
+                    for data in r.iter_content(chunk_size=16384):
+                        dl += len(data)
+                        outfile.write(data)
+                        done = int(dl / total_length * 100)
+                        print(f'{done}%', end='\r')
